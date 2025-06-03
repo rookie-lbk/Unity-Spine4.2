93179e0bf7b11a0e143560ef8b9c74932fe931c6
liubenkang <benkang.liu@centurygame.com>
2025-06-03 14:18:54 +0800

    用于测试Skin的脚本

---
+++ Test_Spine4.2/Assets/Scripts/Test.cs
@@ -0,0 +1,112 @@
+using System.Collections.Generic;
+using Spine;
+using Spine.Unity;
+using Spine.Unity.AttachmentTools;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    SkeletonAnimation skeletonAnimation;
+    Skeleton skeleton;
+
+    [SpineSkin]
+    public string[] addSkinNames = null;
+    public SpineAtlasAsset[] addSkinAssets = null;
+
+    public int loadCount = 0;
+
+    public Texture2D runtimeAtlas;
+    public Material runtimeMaterial;
+
+    void Awake()
+    {
+        skeletonAnimation = GetComponent<SkeletonAnimation>();
+        skeleton = skeletonAnimation.Skeleton;
+
+        // Output all slot names separated by commas
+        var slotNames = new List<string>();
+        var slotAttachmentNames = new List<string>();
+        foreach (var slot in skeleton.Data.Slots)
+        {
+            slotNames.Add(slot.Name);
+            slotAttachmentNames.Add(slot.AttachmentName);
+        }
+        Debug.Log("******Slots: " + string.Join(", ", slotNames));
+        Debug.Log("******SlotAttachmentNames: " + string.Join(", ", slotAttachmentNames));
+
+        // Output all skin names separated by commas
+        var skinNames = new List<string>();
+        foreach (var skin in skeleton.Data.Skins)
+        {
+            skinNames.Add(skin.Name);
+        }
+        Debug.Log("******Skins: " + string.Join(", ", skinNames));
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Skin skinMix = new Skin("skin-mix");
+
+        for (int i = 0; i < addSkinNames.Length; i++)
+        {
+            if (i < loadCount)
+            {
+                Skin addSkin = skeleton.Data.FindSkin(addSkinNames[i]);
+                SetSpineAtlasAsset(addSkinAssets[i], addSkin);
+                // skinMix.AddSkin(addSkin);
+                skinMix.CopySkin(addSkin);
+            }
+        }
+
+        for (int i = 0; i < addSkinAssets.Length; i++)
+        {
+            if (i < loadCount)
+            {
+            }
+        }
+
+        if (runtimeMaterial)
+            Destroy(runtimeMaterial);
+        if (runtimeAtlas)
+            Destroy(runtimeAtlas);
+
+        var skinMixTmp = skinMix.GetRepackedSkin("RepackedSkin", skeletonAnimation.SkeletonDataAsset.atlasAssets[0].PrimaryMaterial, out runtimeMaterial, out runtimeAtlas, maxAtlasSize: 2048);
+        skinMix.Clear();
+
+        skeletonAnimation.Skeleton.Skin = skinMixTmp;
+        // skeleton.SetSkin(skinMixTmp);
+        // skeleton.SetSkin(skinMix);
+
+        skeleton.UpdateCache();
+        skeleton.SetSlotsToSetupPose();
+        skeletonAnimation.AnimationState.Apply(skeleton);
+
+        AtlasUtilities.ClearCache();
+        Resources.UnloadUnusedAssets();
+    }
+
+    void SetSpineAtlasAsset(SpineAtlasAsset spineAtlasAsset, Skin skinMix)
+    {
+        float scale = skeletonAnimation.skeletonDataAsset.scale;
+        Atlas atlas = spineAtlasAsset.GetAtlas();
+        foreach (var region in atlas.Regions)
+        {
+            // Debug.Log("111 region.name: " + region.name);
+            SlotData slotData = skeleton.Data.FindSlot(region.name);
+            if (slotData != null)
+            {
+                // Debug.Log("222 slotData.Name: " + slotData.Name + " slotData.Index: " + slotData.Index);
+                string attachmentName = region.name;
+                Attachment attachment = skinMix.GetAttachment(slotData.Index, attachmentName);
+                if (attachment != null)
+                {
+                    // Debug.Log("333 attachment: " + attachment);
+                    Attachment newAttachment = attachment.GetRemappedClone(region, true, true, scale);
+                    skinMix.SetAttachment(slotData.Index, attachmentName, newAttachment);
+                }
+            }
+        }
+    }
+}
