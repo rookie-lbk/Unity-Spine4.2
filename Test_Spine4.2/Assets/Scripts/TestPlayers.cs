f488c4d87cf68da9ec1a6ae16473a3753e040d35
liubenkang <benkang.liu@centurygame.com>
2025-06-06 17:19:53 +0800

    TestSpine

--- Test_Spine4.2/Assets/Scripts/TestPlayers.cs
+++ Test_Spine4.2/Assets/Scripts/TestPlayers.cs
@@ -9,12 +9,18 @@ public class TestPlayers : MonoBehaviour
     [SerializeField]
     private GameObject playerPrefab;
     [SerializeField]
-    private Text playerCountText;
+    private Text infoText;
     private List<Transform> targetPoints = new();
 
     private List<GameObject> players = new();
     private float targetUpdateTimer = 0f;
     private float targetUpdateInterval;
+
+    void Awake()
+    {
+
+        Application.targetFrameRate = 60;
+    }
 
     void Start()
     {
@@ -22,6 +28,7 @@ public class TestPlayers : MonoBehaviour
         {
             targetPoints.Add(transform.GetChild(i));
         }
+        _timeLeft = _updateInterval;
     }
 
     public void CreatePlayer()
@@ -66,12 +73,39 @@ public class TestPlayers : MonoBehaviour
             }
         }
     }
+    float _updateInterval = 1f;//设定更新帧率的时间间隔为1秒  
+    float _accum = .0f;//累积时间  
+    int _frames = 0;//在_updateInterval时间内运行了多少帧  
+    float _timeLeft;
+    string fpsFormat;
+
+    void UpdateFPS()
+    {
+        _timeLeft -= Time.deltaTime;
+        //Time.timeScale可以控制Update 和LateUpdate 的执行速度,  
+        //Time.deltaTime是以秒计算，完成最后一帧的时间  
+        //相除即可得到相应的一帧所用的时间  
+        _accum += Time.timeScale / Time.deltaTime;
+        ++_frames;//帧数  
+
+        if (_timeLeft <= 0)
+        {
+            float fps = _accum / _frames;
+            //Debug.Log(_accum + "__" + _frames);  
+            fpsFormat = System.String.Format("{0:F2}", fps);//保留两位小数  
+
+            _timeLeft = _updateInterval;
+            _accum = .0f;
+            _frames = 0;
+        }
+    }
 
     void Update()
     {
-        if (playerCountText)
+        UpdateFPS();
+        if (infoText)
         {
-            playerCountText.text = players.Count.ToString();
+            infoText.text = $"Count: {players.Count}\nFPS: {fpsFormat}";
         }
         if (players.Count == 0)
         {
@@ -89,7 +123,7 @@ public class TestPlayers : MonoBehaviour
 
     private void SetRandomUpdateInterval()
     {
-        targetUpdateInterval = Random.Range(1f, 2f);
+        targetUpdateInterval = Random.Range(4f, 8f);
     }
 
     private void RandomPlayerTarget()
